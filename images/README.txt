6a66a5e50f5c047d263a3de78a81758cc0d47127
Judd Devermont <j.devermont@kupanda.com>
2026-03-05 16:09:46 +0000

    Initial commit: Drago Intelligence website with web-safe filenames

---
+++ images/README.txt
@@ -0,0 +1,30 @@
+DRAGO Platform - Leader Photos
+================================
+
+Add your leader photos to this folder with the following filenames:
+
+CURRENT LEADERS:
+- matlanyane.jpg    (Retšelisitsoe Matlanyane - Lesotho Finance Minister)
+- ramaphosa.jpg     (Cyril Ramaphosa - South Africa President)
+
+PHOTO SPECIFICATIONS:
+- Recommended size: 400x400 pixels (square)
+- Minimum size: 200x200 pixels
+- Format: JPG or PNG
+- The photos will be displayed as squares with rounded corners
+
+ADDING NEW LEADERS:
+When you add a new leader to the platform, add their photo here with a
+matching filename. For example, if you add a leader with id: 'biden',
+name the photo: biden.jpg
+
+In the JavaScript code, set:
+  photo: 'images/biden.jpg',
+
+FILE STRUCTURE:
+your-folder/
+  ├── drago-platform.html
+  └── images/
+      ├── matlanyane.jpg
+      ├── ramaphosa.jpg
+      └── [other-leader].jpg
---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'drago-v1';
+const urlsToCache = [
+  '/',
+  '/drago-platform.html',
+  '/about.html',
+  '/manifest.json',
+  '/images/drago-logo.png',
+  '/images/dragologo.jpg',
+  '/images/worldmap.jpg',
+  '/images/profilesworldmap.jpg'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache, falling back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          // Don't cache non-successful responses
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // Clone and cache the response
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
